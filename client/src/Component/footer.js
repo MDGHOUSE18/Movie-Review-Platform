05eabd63a9b40f8019f1d2ca838f3b371cf32bac
MDGHOUSE18 <mdghouse23102@gmail.com>
2022-10-12 11:12:05 +0530

    first commit

---
+++ client/src/Component/footer.js
@@ -0,0 +1,20 @@
+import './mystyle.css'
+export default function footer() {
+  return (
+    <div class="container-fluid bg1">
+      <footer class="site-footer">
+        
+        <h5 class="footer-style">
+          Review My Show.com | Developed by&nbsp;
+          <a 
+            class="text-warning text-decoration-none"
+            href="https://www.linkedin.com/in/mahammed-ghouse-05b1a4203/"
+            target="_blank"
+          >
+            Mahammed Ghouse
+          </a>
+        </h5>
+      </footer>
+    </div>
+  );
+}
